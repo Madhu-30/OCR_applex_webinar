60a7ebb2c8c538dd7930c35bf48aa22cc76a27c4
prajata22 <prajatasamanta22@gmail.com>
2020-08-30 13:38:27 +0530

    webinar day 2

--- app/src/main/java/com/applex/ocrapplex/MainActivity.java
+++ app/src/main/java/com/applex/ocrapplex/MainActivity.java
@@ -1,15 +1,111 @@
 package com.applex.ocrapplex;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class MainActivity extends AppCompatActivity
 {
+    ImageView preview;
+    EditText result;
+    Button select_image;
+
+    String[] storagePermission;
+    private static final int IMAGE_PICK_GALLERY_CODE = 7;
+    private static final int STORAGE_REQUEST_CODE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preview = findViewById(R.id.preview);
+        result = findViewById(R.id.result);
+        select_image = findViewById(R.id.select_image);
+
+        storagePermission = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        select_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkStoragePermission()) {
+                    requestStoragePermission();
+                }
+                else {
+                    pickGallery();
+                }
+            }
+        });
+    }
+
+    private boolean checkStoragePermission() {
+        return ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(MainActivity.this, storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == STORAGE_REQUEST_CODE) {
+            if(grantResults.length > 0) {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickGallery();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void pickGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), IMAGE_PICK_GALLERY_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == IMAGE_PICK_GALLERY_CODE && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            CropImage.activity(uri)
+                    .setActivityTitle("OCR Crop")
+                    .setCropMenuCropButtonTitle("Crop")
+                    .setMultiTouchEnabled(true)
+                    .setAllowRotation(true)
+                    .setAllowCounterRotation(true)
+                    .setAllowFlipping(true)
+                    .setAutoZoomEnabled(true)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(MainActivity.this);
+        }
+        else if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult uri = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK) {
+                preview.setImageURI(uri.getUri());
+            }
+        }
     }
 }
